43bc582704a61a06fffca7d147579e17bdf881c4
Shunadesu <namp280918@gmail.com>
2024-11-11 02:15:30 +0700

    build: sth

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,8 +8,18 @@ import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div
+      className={`xl:mt-12 w-full flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}
+    >
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+      >
+        <p className={style.sectionSubText}>Get in touch</p>
+        <h3 className={style.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
   )
 }
 
-export default Contact
+export default SectionWrapper(Contact, "contact")
